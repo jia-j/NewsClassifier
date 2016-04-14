a87aab346cfadbd3c23b3f3d9520305b08fde82c
sunyam <sunyambagga@gmail.com>
2016-04-14 19:50:03 +0530

    implementing k-NN algorithm

--- classifier.py
+++ classifier.py
@@ -1,6 +1,7 @@
 from News_Scraper import TH_Scraper, W_Scraper, HindustanTimes
 from summary_mod import get_features
 from collections import defaultdict
+from heapq import nlargest
 
 h_url_nontech = 'http://www.thehindu.com/sport'
 h_url_tech = 'http://www.thehindu.com/sci-tech/'
@@ -28,19 +29,20 @@ w_url_tech = 'https://www.washingtonpost.com/business/technology/'
 
 
 
-# WASHINGTON
+# WashingtonPost
 tech_summaries = W_Scraper(w_url_tech, 1)
-
 nontech_summaries = W_Scraper(w_url_nontech, 0)
 
+#print tech_summaries
+#print "\n\n\n\n\n\n"
+#print nontech_summaries
+
 features = []
-main_dict = defaultdict(int)
+main_dict = {}
 
 for techUrl in tech_summaries:
     features = get_features(tech_summaries[techUrl], 25)
-    
     main_dict[techUrl] = {'feature_vector':features, 'label':'Tech'}
-
 
 for nontechUrl in nontech_summaries:
     features = get_features(nontech_summaries[nontechUrl], 25)
@@ -48,20 +50,31 @@ for nontechUrl in nontech_summaries:
 
 print main_dict
 
-#article = "The first international conference on the subject of gravitational wave (GW) astronomy after the discovery of gravitational waves was announced, in February, was held recently at International Centre for Theoretical Sciences (ICTS), Bengaluru. Preceded by a discussion meeting which was exclusively for experts in the field, the conference entitled 'Future of Gravitational Wave Astronomy,' aimed at setting down future paths for gravitational wave researchers. Speaking to this correspondent along the sidelines of the conference, Dr P Ajith of ICTS, the organiser of the conference said, 'The big discovery [of gravitational waves], made last year and announced in February, essentially marks the end of an era - the quest for the detection of gravitational waves. It has also opened out a new branch of astronomy. Now, the idea is to make detection of gravitational waves a tool of gravitational wave astronomy. This can test our understanding of fundamental physics, astronomy, cosmology etc. It marks the beginning of a new era.'"
+#article_body = "Since 2011, Facebook has been rolling out translation features backed by artificial intelligence algorithms. The A.I. reads the post or comment, parses what's being said, and then translates it into a hopefully natural-sounding translation at the click of the button. Last year, instead of asking users if they wanted to have a post translated, Facebook started automatically showing translations, and asking if users want to see the original. Now, more and more Facebook posts are automatically translated; 800 million users per month use the translation feature. But behind the scenes, Facebook is running low-level artificial intelligence on all the text uploaded to Facebook, and documenting how you interact with each language."
 #
-#
-#
-## k-NN
-#test_features = get_features(article, 25)
-#
-#similar = defaultdict(int)
-#
-#for article_url in main_dict:
-#    similar[article_url] = len(set(main_dict[article_url]['feature_vector']).intersection(set(test_features)))
-#
-#print similar
+#article_title = "Facebook loves AI"
+#article = (article_title, article_body)
 
+article = HindustanTimes("http://www.hindustantimes.com/tech/facebook-has-a-new-research-lab-led-by-ex-google-executive/story-7YYln1vKdh3tMihW6rZFNK.html")
 
+# k-NN
+test_features = get_features(article, 25)
+print "TEST FEATURES:\n\n\n\n", test_features
 
+similar = {}
+
+for article_url in main_dict:
+    similar[article_url] = len(set(main_dict[article_url]['feature_vector']).intersection(set(test_features)))
+
+print "SIMILAR DICT:\n\n"
+print similar
+
+knn = nlargest(5, similar, key=similar.get)
+category = defaultdict(int)
+
+for url_neighbor in knn:
+    category[main_dict[url_neighbor]['label']] += 1
+
+print "\n\n\n\n\n"
+print category
 
