a9133434c4c556eb2ccbaee9bf3abfd9df7cf106
sunyam <sunyambagga@gmail.com>
2016-04-17 18:28:33 +0530

    creating tech and non-tech corpora

--- classifier.py
+++ classifier.py
@@ -1,14 +1,60 @@
-from News_Scraper import TH_Scraper, W_Scraper, HindustanTimes
 from summary_mod import get_features
+
 from collections import defaultdict
 from heapq import nlargest
+import csv
 
-h_url_nontech = 'http://www.thehindu.com/sport'
-h_url_tech = 'http://www.thehindu.com/sci-tech/'
-w_url_nontech = 'https://www.washingtonpost.com/sports'
-w_url_tech = 'https://www.washingtonpost.com/business/technology/'
+#features = []
+#main_dict = {}
+#
+#for website_dict in [w_tech_articles, ny_tech_articles]:
+#    print "\n\nInside Tech"
+#    for techUrl in website_dict:
+#        features = get_features(website_dict[techUrl], 25)
+#        main_dict[techUrl] = {'feature_vector':features, 'label':'Tech'}
+#
+#for website_dict in [w_nontech_articles, ny_nontech_articles]:
+#    print "\n\nInside Non-Tech"
+#    for nontechUrl in website_dict:
+#        features = get_features(website_dict[nontechUrl], 25)
+#        main_dict[nontechUrl] = {'feature_vector':features, 'label':'Non-Tech'}
+#
+#print main_dict
+#
+#with open('features.csv', 'wb') as file:
+#    writer = csv.writer(file, delimiter=',', quotechar='"')
+#    print "WRITING TO CSV"
+#    for url in main_dict:
+#        writer.writerow([main_dict[url]['feature_vector'], main_dict[url]['label']])
+#
+#print "\nDONE!"
+#article = HindustanTimes("http://www.hindustantimes.com/tech/facebook-has-a-new-research-lab-led-by-ex-google-executive/story-7YYln1vKdh3tMihW6rZFNK.html")
+
+# k-NN
+#test_features = get_features(article, 25)
+#print "TEST FEATURES:\n\n\n\n", test_features
+#
+#similar = {}
+#
+#for article_url in main_dict:
+#    similar[article_url] = len(set(main_dict[article_url]['feature_vector']).intersection(set(test_features)))
+#
+#print "SIMILAR DICT:\n\n"
+#print similar
+#
+#knn = nlargest(5, similar, key=similar.get)
+#category = defaultdict(int)
+#
+#for url_neighbor in knn:
+#    category[main_dict[url_neighbor]['label']] += 1
+#
+#print "\n\n\n\n\n"
+#print category
+
 
 # TheHindu
+#h_url_nontech = 'http://www.thehindu.com/sport'
+#h_url_tech = 'http://www.thehindu.com/sci-tech/'
 #tech_summaries = TH_Scraper(h_url_tech, 1)
 #
 #nontech_summaries = TH_Scraper(h_url_nontech, 0)
@@ -28,53 +74,4 @@ w_url_tech = 'https://www.washingtonpost.com/business/technology/'
 
 
 
-
-# WashingtonPost
-tech_summaries = W_Scraper(w_url_tech, 1)
-nontech_summaries = W_Scraper(w_url_nontech, 0)
-
-#print tech_summaries
-#print "\n\n\n\n\n\n"
-#print nontech_summaries
-
-features = []
-main_dict = {}
-
-for techUrl in tech_summaries:
-    features = get_features(tech_summaries[techUrl], 25)
-    main_dict[techUrl] = {'feature_vector':features, 'label':'Tech'}
-
-for nontechUrl in nontech_summaries:
-    features = get_features(nontech_summaries[nontechUrl], 25)
-    main_dict[nontechUrl] = {'feature_vector':features, 'label':'Non-Tech'}
-
-print main_dict
-
-#article_body = "Since 2011, Facebook has been rolling out translation features backed by artificial intelligence algorithms. The A.I. reads the post or comment, parses what's being said, and then translates it into a hopefully natural-sounding translation at the click of the button. Last year, instead of asking users if they wanted to have a post translated, Facebook started automatically showing translations, and asking if users want to see the original. Now, more and more Facebook posts are automatically translated; 800 million users per month use the translation feature. But behind the scenes, Facebook is running low-level artificial intelligence on all the text uploaded to Facebook, and documenting how you interact with each language."
-#
-#article_title = "Facebook loves AI"
-#article = (article_title, article_body)
-
-article = HindustanTimes("http://www.hindustantimes.com/tech/facebook-has-a-new-research-lab-led-by-ex-google-executive/story-7YYln1vKdh3tMihW6rZFNK.html")
-
-# k-NN
-test_features = get_features(article, 25)
-print "TEST FEATURES:\n\n\n\n", test_features
-
-similar = {}
-
-for article_url in main_dict:
-    similar[article_url] = len(set(main_dict[article_url]['feature_vector']).intersection(set(test_features)))
-
-print "SIMILAR DICT:\n\n"
-print similar
-
-knn = nlargest(5, similar, key=similar.get)
-category = defaultdict(int)
-
-for url_neighbor in knn:
-    category[main_dict[url_neighbor]['label']] += 1
-
-print "\n\n\n\n\n"
-print category
 
---
+++ create_corpora.py
@@ -0,0 +1,92 @@
+from News_Scraper import TH_Scraper, W_Scraper, NYT_Scraper, HT_Scraper
+import csv
+
+def createCorpora():
+    
+    techCount = 1
+    nontechCount = 1
+    tech_path = '/Users/sunyambagga/GitHub/NewsClassifier/corpora/tech/'
+    nontech_path = '/Users/sunyambagga/GitHub/NewsClassifier/corpora/non_tech/'
+    
+    # WashingtonPost
+    w_url_nontech = 'https://www.washingtonpost.com/sports'
+    w_url_tech = 'https://www.washingtonpost.com/business/technology/'
+    w_tech_articles = W_Scraper(w_url_tech, 1)
+    w_nontech_articles = W_Scraper(w_url_nontech, 0)
+    print "\n\nWashington: Done"
+
+    # NYTimes
+    ny_url_nontech = 'http://www.nytimes.com/pages/sports/index.html'
+    ny_url_tech = 'http://www.nytimes.com/pages/technology/index.html'
+    ny_tech_articles = NYT_Scraper(ny_url_tech, 1)
+    ny_nontech_articles = NYT_Scraper(ny_url_nontech, 0)
+    print "\n\nNYTimes: Done"
+
+    # HindustanTimes
+    ht_url_nontech = 'http://www.hindustantimes.com/sports/'
+    ht_url_tech = 'http://www.hindustantimes.com/tech/'
+    ht_tech_articles = HT_Scraper(ht_url_tech, 1)
+    ht_nontech_articles = HT_Scraper(ht_url_nontech, 0)
+    print "\n\nHindustanTimes: Done"
+    
+    # Creating corpora by writing to file
+    # Non-Tech
+    for url in w_nontech_articles:
+        title = w_nontech_articles[url][0]
+        body = w_nontech_articles[url][1]
+        article = title + "\n" + body
+        article = article.encode('utf8')
+        with open(nontech_path+'washington_'+str(nontechCount)+'.txt', 'wb') as file:
+            file.write(article)
+            nontechCount += 1
+
+    for url in ny_nontech_articles:
+        title = ny_nontech_articles[url][0]
+        body = ny_nontech_articles[url][1]
+        article = title + "\n" + body
+        article = article.encode('utf8')
+        
+        with open(nontech_path+'nytimes_'+str(nontechCount)+'.txt', 'wb') as file:
+            file.write(article)
+            nontechCount += 1
+
+    for url in ht_nontech_articles:
+        title = ht_nontech_articles[url][0]
+        body = ht_nontech_articles[url][1]
+        article = title + "\n" + body
+        article = article.encode('utf8')
+        with open(nontech_path+'ht_'+str(nontechCount)+'.txt', 'wb') as file:
+            file.write(article)
+            nontechCount += 1
+
+
+    # Tech
+    for url in w_tech_articles:
+        title = w_tech_articles[url][0]
+        body = w_tech_articles[url][1]
+        article = title + "\n" + body
+        article = article.encode('utf8')
+        with open(tech_path+'washington_'+str(techCount)+'.txt', 'wb') as file:
+            file.write(article)
+            techCount += 1
+
+    for url in ny_tech_articles:
+        title = ny_tech_articles[url][0]
+        body = ny_tech_articles[url][1]
+        article = title + "\n" + body
+        article = article.encode('utf8')
+        with open(tech_path+'nytimes_'+str(techCount)+'.txt', 'wb') as file:
+            file.write(article)
+            techCount += 1
+
+    for url in ht_tech_articles:
+        title = ht_tech_articles[url][0]
+        body = ht_tech_articles[url][1]
+        article = title + "\n" + body
+        article = article.encode('utf8')
+        with open(tech_path+'ht_'+str(techCount)+'.txt', 'wb') as file:
+            file.write(article)
+            techCount += 1
+
+
+#createCorpora()
