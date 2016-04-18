5b10c2a4b07667f31de9a1b65cc6a4fac5ea9426
sunyam <sunyambagga@gmail.com>
2016-04-18 15:03:29 +0530

    Preparing feature vector

--- classifier.py
+++ classifier.py
@@ -2,32 +2,58 @@ from summary_mod import get_features
 
 from collections import defaultdict
 from heapq import nlargest
-import csv
 
-#features = []
-#main_dict = {}
-#
-#for website_dict in [w_tech_articles, ny_tech_articles]:
-#    print "\n\nInside Tech"
-#    for techUrl in website_dict:
-#        features = get_features(website_dict[techUrl], 25)
-#        main_dict[techUrl] = {'feature_vector':features, 'label':'Tech'}
-#
-#for website_dict in [w_nontech_articles, ny_nontech_articles]:
-#    print "\n\nInside Non-Tech"
-#    for nontechUrl in website_dict:
-#        features = get_features(website_dict[nontechUrl], 25)
-#        main_dict[nontechUrl] = {'feature_vector':features, 'label':'Non-Tech'}
-#
-#print main_dict
-#
-#with open('features.csv', 'wb') as file:
-#    writer = csv.writer(file, delimiter=',', quotechar='"')
-#    print "WRITING TO CSV"
-#    for url in main_dict:
-#        writer.writerow([main_dict[url]['feature_vector'], main_dict[url]['label']])
-#
-#print "\nDONE!"
+import csv
+import os
+
+features = []
+
+main_list = []
+# This is the main dictionary: {'url':{'feature_vector':[], 'label':'Tech'}..}
+
+#--------------------------------------------------#
+def prepareFeatures_csv():
+    nonTechPath = '/Users/sunyambagga/GitHub/NewsClassifier/corpora/nonTech/'
+    techPath = '/Users/sunyambagga/GitHub/NewsClassifier/corpora/tech/'
+
+    for file_name in os.listdir(nonTechPath):
+        if '.txt' in file_name:
+            print "Reading file: ", file_name
+            with open(nonTechPath+file_name, 'rb') as file:
+                tuple = file.readlines()
+                tuple[0] = tuple[0].decode("ascii", errors="ignore")
+                tuple[1] = tuple[1].decode("ascii", errors="ignore")
+                
+                features = get_features(tuple, 25)
+                main_list.append({'feature_vector':features, 'label':'Non-Tech'})
+
+    print "\n\n"
+    
+    for file_name in os.listdir(techPath):
+        if '.txt' in file_name:
+            print "Reading file: ", file_name
+            with open(techPath+file_name, 'rb') as file:
+                tuple = file.readlines()
+                tuple[0] = tuple[0].decode("ascii", errors="ignore")
+                tuple[1] = tuple[1].decode("ascii", errors="ignore")
+                
+                features = get_features(tuple, 25)
+                main_list.append({'feature_vector':features, 'label':'Tech'})
+
+    print main_list
+
+    with open('latest_features.csv', 'wb') as file:
+        writer = csv.writer(file, delimiter=',', quotechar='"')
+        print "Writing features to CSV"
+        for dict in main_list:
+            writer.writerow([dict['feature_vector'], dict['label']])
+
+    print "\nDONE!"
+
+#prepareFeatures_csv()
+#--------------------------------------------------#
+
+
 #article = HindustanTimes("http://www.hindustantimes.com/tech/facebook-has-a-new-research-lab-led-by-ex-google-executive/story-7YYln1vKdh3tMihW6rZFNK.html")
 
 # k-NN
@@ -36,8 +62,8 @@ import csv
 #
 #similar = {}
 #
-#for article_url in main_dict:
-#    similar[article_url] = len(set(main_dict[article_url]['feature_vector']).intersection(set(test_features)))
+#for article_url in main_list:
+#    similar[article_url] = len(set(main_list[article_url]['feature_vector']).intersection(set(test_features)))
 #
 #print "SIMILAR DICT:\n\n"
 #print similar
@@ -46,7 +72,7 @@ import csv
 #category = defaultdict(int)
 #
 #for url_neighbor in knn:
-#    category[main_dict[url_neighbor]['label']] += 1
+#    category[main_list[url_neighbor]['label']] += 1
 #
 #print "\n\n\n\n\n"
 #print category
@@ -60,17 +86,17 @@ import csv
 #nontech_summaries = TH_Scraper(h_url_nontech, 0)
 #
 #features = []
-#main_dict = defaultdict(int)
+#main_list = defaultdict(int)
 #
 #for techUrl in tech_summaries:
 #    features = get_features(tech_summaries[techUrl], 25)
 #    
-#    main_dict[techUrl] = {'feature_vector':features, 'label':'Tech'}
+#    main_list[techUrl] = {'feature_vector':features, 'label':'Tech'}
 #
 #
 #for nontechUrl in nontech_summaries:
 #    features = get_features(nontech_summaries[nontechUrl], 25)
-#    main_dict[nontechUrl] = {'feature_vector':features, 'label':'Non-Tech'}
+#    main_list[nontechUrl] = {'feature_vector':features, 'label':'Non-Tech'}
 
 
 
